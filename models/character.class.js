e0de12a6ade70f77f217480a92936783efc26c14
Ricardo Geada <142402302+RicardoGeada@users.noreply.github.com>
2023-10-25 11:36:51 +0200

    first walk animation added

--- models/character.class.js
+++ models/character.class.js
@@ -2,10 +2,28 @@ class Character extends MovableObject {
     width = 32;
     height = 32;
 
+    IMAGES_WALKING = [
+        'img/1.hero/Walk/walk_1.png',
+        'img/1.hero/Walk/walk_2.png',
+        'img/1.hero/Walk/walk_3.png',
+        'img/1.hero/Walk/walk_4.png',
+    ];
+
     constructor() {
         super().loadImage('img/1.hero/Idle/idle_1.png');
+        this.loadImages(this.IMAGES_WALKING);
         this.x = 10;
         this.y = 208 - 32 - 16;
+
+        this.animate(this.IMAGES_WALKING);
+    }
+
+    animate(stateImages) { 
+        setInterval(() => {
+            let path = stateImages[this.currentImage];
+            this.img = this.imageCache[path];
+            this.currentImage == stateImages.length - 1 ? this.currentImage = 0 : this.currentImage++;
+        }, 1000 / 4);
     }
 
     jump() {
--- models/enemie-robo-totem.class.js
+++ models/enemie-robo-totem.class.js
@@ -1,10 +1,32 @@
 class RoboTotem extends MovableObject{
 
+    IMAGES_WALKING = [
+        'img/2.enemies/robo_totem/armored/Walk/walk_1.png',
+        'img/2.enemies/robo_totem/armored/Walk/walk_2.png',
+    ]
+
     constructor() {
         super().loadImage('img/2.enemies/robo_totem/armored/Standing/standing.png');
+        this.loadImages(this.IMAGES_WALKING)
         this.x = 100 + Math.random() * 500;
         this.y = 208 - 32 - 16;
         this.width = 16;
         this.height = 32;
+        this.animate(this.IMAGES_WALKING);
+        // this.movement(); 
     }
+
+    animate(stateImages) { 
+        setInterval(() => {
+            let path = stateImages[this.currentImage];
+            this.img = this.imageCache[path];
+            this.currentImage == stateImages.length - 1 ? this.currentImage = 0 : this.currentImage++;
+        }, 1000 / 4);
+    }
+
+    // movement() {
+    //     setInterval(() => {
+    //         this.img.src.includes('walk_1') ? this.img.src = this.IMAGES_WALKING[1] : this.img.src = this.IMAGES_WALKING[0];
+    //     }, 1000 / 3);
+    // }
 }
